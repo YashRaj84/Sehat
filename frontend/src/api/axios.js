8d03df2fdc0bec10e4caf58c8a98d3c7e9525696
Yash Raj <yash26988412@gmail.com>
2026-02-24 10:42:00 +0530

    Updated API URL for production

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://sehat-h4to.onrender.com/api",
 });
 
 api.interceptors.request.use((req) => {
